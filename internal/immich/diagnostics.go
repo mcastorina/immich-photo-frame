6a75f273765ef2cc10617e9c92065070cf8ef6bf
Miccah Castorina <m.castorina93@gmail.com>
2026-01-02 11:19:02 -0800

    Change RemoteConnectedError to a string

--- internal/immich/diagnostics.go
+++ internal/immich/diagnostics.go
@@ -5,18 +5,21 @@ type ClientDiagnostics struct {
 	LocalConfigured      bool
 	InMemoryConfigured   bool
 	RemoteConfigured     bool
-	RemoteConnectedError error
+	RemoteConnectedError string
 }
 
 // Diagnostics reports how the client is configured and checks if the remote is connected.
 //
 // TODO: Add in-memory and local information like memory used / configured.
 func (c Client) Diagnostics() ClientDiagnostics {
-	diagnostics := ClientDiagnostics{}
-	diagnostics.LocalConfigured = (c.local != nil)
-	diagnostics.InMemoryConfigured = (c.cache != nil)
-	diagnostics.RemoteConfigured = (c.remote != nil)
-	diagnostics.RemoteConnectedError = c.remote.IsConnected()
-
-	return diagnostics
+	remoteConnected := ""
+	if err := c.remote.IsConnected(); err != nil {
+		remoteConnected = err.Error()
+	}
+	return ClientDiagnostics{
+		LocalConfigured:      (c.local != nil),
+		InMemoryConfigured:   (c.cache != nil),
+		RemoteConfigured:     (c.remote != nil),
+		RemoteConnectedError: remoteConnected,
+	}
 }
