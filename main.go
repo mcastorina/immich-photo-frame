b5f50fee53017b0f50687445af866478af4b4d96
Miccah Castorina <m.castorina93@gmail.com>
2026-01-17 11:45:05 -0800

    Add --debug flag

--- main.go
+++ main.go
@@ -1,6 +1,8 @@
 package main
 
 import (
+	"errors"
+	"fmt"
 	"log/slog"
 	"os"
 
@@ -8,11 +10,35 @@ import (
 )
 
 func main() {
-	logger := slog.New(slog.NewJSONHandler(os.Stderr, nil))
+	programLevel := new(slog.LevelVar)
+	logger := slog.New(slog.NewJSONHandler(os.Stderr, &slog.HandlerOptions{Level: programLevel}))
 	slog.SetDefault(logger)
+
+	if ok, err := debugArgSet(os.Args[1:]); err != nil {
+		slog.Error("argparse failed", "error", err)
+		os.Exit(1)
+	} else if ok {
+		programLevel.Set(slog.LevelDebug)
+	}
 
 	if err := app.Run(); err != nil {
 		slog.Error("app failed", "error", err)
 		os.Exit(1)
 	}
+}
+
+// debugArgSet checks the list of program arguments (without the program name)
+// for the --debug flag. If an unexpected number of args is passed, or the flag
+// is not "--debug", an error is returned.
+func debugArgSet(args []string) (bool, error) {
+	if len(args) == 0 {
+		return false, nil
+	} else if len(args) != 1 {
+		return false, errors.New("too many arguments")
+	}
+
+	if args[0] != "--debug" {
+		return false, fmt.Errorf(`unrecognized arg %q, expected "--debug"`, args[0])
+	}
+	return true, nil
 }
