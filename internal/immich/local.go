efa787fd999c962eae263c333f8827dd5d64847f
Miccah Castorina <m.castorina93@gmail.com>
2026-01-18 11:48:02 -0800

    Limit storage space in the local storage client
    This makes space for an eviction policy, however the current
    implementation simply stops writing to the directory if we run out of
    space.

--- internal/immich/local.go
+++ internal/immich/local.go
@@ -2,8 +2,14 @@ package immich
 
 import (
 	"encoding/json"
+	"errors"
+	"fmt"
+	"io/fs"
+	"log/slog"
 	"os"
 	"path/filepath"
+
+	"github.com/dustin/go-humanize"
 )
 
 // localStorageClient is a rwClient for storing and retrieving assets and
@@ -92,8 +98,73 @@ func (l localStorageClient) get(key string) ([]byte, error) {
 // store is a helper method to convert the key to a filepath and write the data
 // to disk.
 func (l localStorageClient) store(key string, data []byte) error {
-	// TODO: Manage amount of disk space used.
+	path := filepath.Join(l.conf.LocalStoragePath, key)
+	if hasSpace, err := l.hasSpace(path, int64(len(data))); err == nil && !hasSpace {
+		if err := l.evict(len(data)); err != nil {
+			return fmt.Errorf("not enough space: %w", err)
+		}
+	} else if err != nil {
+		return fmt.Errorf("failed to check local storage space: %w", err)
+	}
 	return os.WriteFile(filepath.Join(l.conf.LocalStoragePath, key), data, 0644)
+}
+
+// hasSpace calculates the amount of space used in the configured path and
+// checks if there is enough space to hold the amount of bytes requested. The
+// path argument is the path in which the amount of bytes will go, so if there
+// is an existing file, we can "reclaim" some of that space.
+func (l localStorageClient) hasSpace(path string, bytesRequested int64) (bool, error) {
+	bytesUsed, err := l.bytesInUse()
+	if err != nil {
+		return false, fmt.Errorf("failed to calculate bytes used: %w", err)
+	}
+
+	bytesToReplace := int64(0)
+	{
+		info, err := os.Stat(path)
+		if err != nil && !errors.Is(err, os.ErrNotExist) {
+			slog.Debug("could not stat file", "path", path, "error", err)
+			return false, fmt.Errorf("failed to get existing bytes for file: %w", err)
+		} else if err == nil {
+			bytesToReplace = info.Size()
+		}
+	}
+
+	slog.Debug("calculated local storage space",
+		"storage_configured", l.conf.LocalStorageSize.String(),
+		"storage_used", humanize.Bytes(uint64(bytesUsed)),
+		"storage_needed", humanize.Bytes(uint64(bytesRequested-bytesToReplace)),
+	)
+	return bytesUsed-bytesToReplace+bytesRequested <= int64(l.conf.LocalStorageSize), nil
+}
+
+// bytesInUse calculates the number of bytes used in the configured path. It
+// takes a best-effort approach and only returns an error if there was an error
+// opening the top-level directory.
+func (l localStorageClient) bytesInUse() (int64, error) {
+	var totalSize int64
+	err := filepath.WalkDir(l.conf.LocalStoragePath, func(path string, d fs.DirEntry, err error) error {
+		// If there was an error opening the root path, surface that error.
+		if path == l.conf.LocalStoragePath && err != nil {
+			return err
+		}
+		// Otherwise, ignore errors and directories.
+		if err != nil || d.IsDir() {
+			return nil
+		}
+		// Ignore any errors to do a best effort approach.
+		if info, err := d.Info(); err == nil {
+			totalSize += info.Size()
+		}
+		return nil
+	})
+	return totalSize, err
+}
+
+// evict attempts to make enough room in the configured directory path to hold
+// nbytes more bytes.
+func (l localStorageClient) evict(nbytes int) error {
+	return errors.New("no eviction policy")
 }
 
 // newInMemoryCacheClient initializes a [localStorageClient] client.
